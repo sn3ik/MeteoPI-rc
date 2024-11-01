ba9bd7f853860067d0798938ac873e371342d007
sn3ik <63190443+sn3ik@users.noreply.github.com>
2024-11-01 13:14:05 +0100

    Update install.py

--- install.py
+++ install.py
@@ -62,7 +62,7 @@ def lighttpd():
     os.system("sudo chmod 777 /var/www/html/include/*")
     os.system("sudo chmod 777 /var/log/lighttpd/")
     # permessi per accedere ai file
-    os.system("sudo chown -R :www-data /var/www/html/")
+    #os.system("sudo chown -R :www-data /var/www/html/")
 
     os.system("sudo service lighttpd force-reload")
     os.chdir("..")
